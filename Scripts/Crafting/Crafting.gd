f39737f2832268682f83373ffb495a618a132b53
Blake Arnold <arnold.blake@gmail.com>
2024-09-13 10:05:15 -0400

    Completed Crafting System

---
+++ Scripts/Crafting/Crafting.gd
@@ -0,0 +1,47 @@
+extends Node
+
+@onready var window: Panel = get_node("CraftingWindow")
+@onready var ui_parent: VBoxContainer = get_node("CraftingWindow/MarginContainer/RecipeContainer")
+
+@export var crafting_recipe_ui_scene: PackedScene
+@export var recipes: Array[CraftingRecipe]
+
+var recipe_uis: Array[CraftingRecipeUI]
+var inventory: Inventory
+
+func _ready() -> void:
+	if window.visible:
+		toggle_window(!window.visible)
+		
+	inventory = get_parent().get_node("Inventory")
+	
+	for recipe in recipes:
+		var recipe_node = crafting_recipe_ui_scene.instantiate()
+		ui_parent.add_child(recipe_node)
+		recipe_node.recipe = recipe
+		recipe_node.crafting = self
+		recipe_uis.append(recipe_node)
+		
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("craft"):
+			toggle_window(!window.visible)
+
+func craft(recipe: CraftingRecipe) -> void:
+	for req in recipe.requirements:
+		for i in range(req.quantity):
+			inventory.remove_item(req.item)
+	inventory.add_item(recipe.item)
+	update_recipe_ui()
+	
+func toggle_window(open:bool) -> void:
+	window.visible = open
+	if open:
+		if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		update_recipe_ui()
+	else:
+		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+		
+func update_recipe_ui() -> void:
+		for recipe in recipe_uis:
+			recipe.update_recipe(inventory)
--- Scripts/Inventory/Inventory.gd
+++ Scripts/Inventory/Inventory.gd
@@ -9,7 +9,8 @@ var slots : Array[InventorySlot]
 @export var starter_items : Array[Item]
 
 func _ready() -> void:
-	toggle_window(!window.visible)
+	if window.visible:
+		toggle_window(!window.visible)
 	for child in get_node("InventoryWindow/InventoryContainer").get_children():
 		child = child as InventorySlot
 		slots.append(child)
@@ -21,7 +22,7 @@ func _ready() -> void:
 		
 	GlobalSignals.on_give_player_item.connect(on_give_player_item)
 	
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("inventory"):
 		toggle_window(!window.visible)
 	
@@ -49,7 +50,7 @@ func add_item(item: Item) -> void:
 	
 func remove_item(item: Item) -> void:
 	var slot = get_slot_to_remove(item)
-	if slot == null or slot.item == item:
+	if slot == null or slot.item != item:
 		return
 	slot.remove_item()
 	
